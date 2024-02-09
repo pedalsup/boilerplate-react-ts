0309f7eb167f27d65fe109655e4825d38fa13b81
rupen-pedalsup <rupen.jarsaniya@pedalsup.com>
2024-02-09 17:04:27 +0530

    :star: feat: implement redux

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,4 @@
 export const ENV = {
   IS_DEV: (process.env.NODE_ENV || '') !== 'production',
   IS_PRODUCTION: process.env.IS_PRODUCTION || false,
-  I18NEXUS_API_KEY: process.env.I18NEXUS_API_KEY || '',
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import { STORE } from './store';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={STORE}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 );
